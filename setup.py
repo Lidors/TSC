e8120ed2889e54bf3ae5f2f516c8daa00ccc0360
Lidors <37260959+Lidors@users.noreply.github.com>
2020-03-09 17:43:07 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 17:30:31 2020
+
+@author: lidor
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TSC-pkg-lidors15", # Replace with your own username
+    version="0.0.1",
+    author="Lidor Spivak",
+    author_email="lidors15@gmail.com",
+    description="Trash Spike classifier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lidors/TSC.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
