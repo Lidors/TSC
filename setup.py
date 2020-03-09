e8f12f4a9a0f5a27a88437b51e59b3a6bb3d4bf9
Lidors <37260959+Lidors@users.noreply.github.com>
2020-03-09 17:57:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TSC-pkg-lidors15", # Replace with your own username
+    name="TSC", # Replace with your own username
     version="0.0.1",
     author="Lidor Spivak",
     author_email="lidors15@gmail.com",
